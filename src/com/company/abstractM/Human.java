ca87f8bcceaf4d7064ebfe3b2d4b65ecf0fbe8f4
Sohan Bhole <101004475+SohanBB@users.noreply.github.com>
2022-03-07 11:25:28 +0530

    Delete src/com/company/abstractM directory

--- src/com/company/abstractM/Human.java
+++
@@ -1,7 +0,0 @@
-package com.company.abstractM;
-
-abstract class Human {
-    public void display(){
-        System.out.println("This is human species: ");
-    }
-}
--- src/com/company/abstractM/Main.java
+++
@@ -1,9 +0,0 @@
-package com.company.abstractM;
-
- class Main extends Human {
-     public static void main (String[] args){
-         Main obj = new Main();
-         obj.display();
-
-     }
-}
