1492cc6a06b0c1b6dfdb06a304ea6ea6c16fb140
SohanBB <sohanbhole999@gmail.com>
2022-04-29 16:30:30 +0530

    Update

--- src/Exercise/Pcheck/Pcheck.java
+++ src/Exercise/Pcheck/Pcheck.java
@@ -3,15 +3,14 @@ package Exercise.Pcheck;
 public class Pcheck {
     public static void main(String[] args) {
         String str = "radar";
-        int n = str.length();
         String rstr = "";
-        for (int i = n-1; i >=0 ; i--) {
-            rstr = rstr+str.charAt(i);
+        int n = str.length();
+        for (int i = n-1; i>= 0 ; i--) {
+            rstr= rstr+str.charAt(i);
         }
-        System.out.println(rstr);
-        if(str.equals(rstr))
-            System.out.println("String is Palindrome");
+        if(rstr.equals(str)){
+            System.out.println("Given string is Palindrome: ");
+        }
         else
-            System.out.println("String is not Palindrome");
-    }
-}
+            System.out.println("Given string is not Palindrome: ");
+}}
--- src/Exercise/PrimeNumber/PrimeNumber.java
+++ src/Exercise/PrimeNumber/PrimeNumber.java
@@ -2,19 +2,19 @@ package Exercise.PrimeNumber;
 
 public class PrimeNumber {
     public static void main(String[] args) {
-        int n=50;
+        int n=20;
         boolean flag = false;
         for (int i = 2; i <=n/2 ; i++) {
-            if(n%i==0){
-                flag = true;
+            if(n%i==0) {
+                flag=true;
                 break;
             }
-
         }
-        if(!flag)
-            System.out.println("Prime number");
+        if(!flag){
+            System.out.println("Number is Prime: ");
+        }
         else
-            System.out.println("Not a prime number");
+            System.out.println("Number is not prime: ");
 
     }
 
