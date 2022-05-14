086187afe4836b81745373966cf76a8ee913351e
SohanBB <sohanbhole999@gmail.com>
2022-05-14 15:30:59 +0530

    Assignments

--- src/DSA/Search/Search.java
+++ src/DSA/Search/Search.java
@@ -1,29 +1,26 @@
 package DSA.Search;
 
 public class Search {
-    public int find(int array[],int x,int low,int high){
-        while (low<=high){
+    static int find(int []data,int x, int low, int high){
+        while(low<=high){
             int mid = low+(high-low)/2;
-            if(array[mid]==x)
+            if (x==data[mid])
                 return mid;
-            if(x<array[mid])
-                high=mid-1;
+            else if(x<data[mid])
+                high = mid-1;
             else
-                low=mid+1;
+                low= mid+1;
         }
         return -1;
     }
-
     public static void main(String[] args) {
-        int [] array={3,6,9,12,15,18};
-        int x= 15;
-        int n = array.length;
-        Search obj = new Search();
-        int result= obj.find(array,x,0,n);
-        if (result==-1){
-            System.out.println("Element is not found");
+        int []data = {1,2,3,4,5,6,7,8};
+        int x= 5;
+        int result= Search.find(data,x,0,data.length-1);
+        if(result == -1){
+            System.out.println("Not found");
         }
         else
-        System.out.println("Element found at index:  "+result);
+            System.out.println(result);
     }
 }
--- src/DSA/Sort/Sort.java
+++ src/DSA/Sort/Sort.java
@@ -3,26 +3,19 @@ package DSA.Sort;
 import java.util.Arrays;
 
 class Sort {
+    static void sorting(int data[]){
+        for (int i = 0; i < data.length; i++) {
+            for (int j = i; j < data.length-i-1 ; j++) {
+                if(data[j+1]<data[j]){
+                    int temp = data[j];
+                    data[j]= data[j+1];
+                    data[j+1]= temp;
+                }}}
+    }
+
     public static void main(String[] args) {
-        int []data = {3,1,5,2,4};
+        int[]data = {1,3,2,5,4,9,6};
         Sort.sorting(data);
         System.out.println(Arrays.toString(data));
-
-
     }
-    static void sorting(int []data){
-        int n = data.length;
-        for (int i = 0; i < n-1; i++) {
-            for (int j = 0; j <n-1-i ; j++) {
-                if(data[j]>data[j+1]){
-                    int temp = data[j];
-                    data[j]=data[j+1];
-                    data[j+1]= temp;
-                }
-
-            }
-
-        }
-    }
-
 }
